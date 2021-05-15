8fb98ff82a31b85ee5dcb9fac714774b2e5abf2c
madhua <madhua@synebytes.com>
2021-05-15 16:48:11 +0530

    Implementing complete logic for game.

--- src/main/java/com/sb/service/GameService.java
+++ src/main/java/com/sb/service/GameService.java
@@ -3,11 +3,14 @@ package com.sb.service;
 import com.sb.game.Die;
 import com.sb.game.GameServiceGrpc;
 import com.sb.game.GameState;
+import com.sb.game.Player;
 import io.grpc.stub.StreamObserver;
 
 public class GameService extends GameServiceGrpc.GameServiceImplBase {
     @Override
     public StreamObserver<Die> roll(StreamObserver<GameState> responseObserver) {
-        return super.roll(responseObserver);
+        Player client = Player.newBuilder().setName("client").setPosition(0).build();
+        Player server = Player.newBuilder().setName("server").setPosition(0).build();
+        return new DieStreamingRequest(client, server, responseObserver);
     }
 }
